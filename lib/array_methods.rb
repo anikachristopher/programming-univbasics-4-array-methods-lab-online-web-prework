4b271b0d46e6db6b86b129b761e9322e157ccb5f
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-26 23:25:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include? ("liking")
+  array,include? ("learning")
 end
 
 def using_sort(array)
